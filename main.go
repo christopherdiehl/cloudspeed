99d247305337b47d5e146907ecb1e14d0c383b6b
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2019-04-25 19:49:50 -0400

    add persist and names to the stack

--- main.go
+++ main.go
@@ -13,6 +13,13 @@ import (
 	"github.com/fatih/color"
 )
 
+// Stack is a helper struct to store  the template body, stack id, and stack name in one place
+type Stack struct {
+	Template string // template body
+	Name     string // name of stack
+	ID       string // id of stack
+}
+
 /*
  * getTemplate reads in the template file specified in location
  * returns the template file as a string or an error
@@ -25,10 +32,10 @@ func getTemplate(location string) (string, error) {
 	return string(b), nil
 }
 
-func createStack(cf *cloudformation.CloudFormation, template string) (string, error) {
+func (stack *Stack) create(cf *cloudformation.CloudFormation) (string, error) {
 	input := &cloudformation.CreateStackInput{
-		StackName:    aws.String("CloudValidateTemp"),
-		TemplateBody: aws.String(template),
+		StackName:    aws.String(stack.Name),
+		TemplateBody: aws.String(stack.Template),
 		Parameters:   nil,
 	}
 	if err := input.Validate(); err != nil {
@@ -42,12 +49,12 @@ func createStack(cf *cloudformation.CloudFormation, template string) (string, er
 	return *output.StackId, nil
 }
 
-// describeStack checks the current status of the stack and returns an error if the stack
-func describeStack(cf *cloudformation.CloudFormation, stackID string) error {
+// describe checks the current status of the stack and returns an error if the stack
+func (stack *Stack) describe(cf *cloudformation.CloudFormation) error {
 	for {
 		out, err := cf.DescribeStacks(&cloudformation.DescribeStacksInput{
 			NextToken: aws.String("1"),
-			StackName: aws.String(stackID),
+			StackName: aws.String(stack.ID),
 		})
 
 		if err != nil {
@@ -62,16 +69,19 @@ func describeStack(cf *cloudformation.CloudFormation, stackID string) error {
 
 }
 
-// deleteStack deletes the current stack and returns an error if failing to do so
-func deleteStack(cf *cloudformation.CloudFormation, stackID string) error {
+// delete deletes the current stack and returns an error if failing to do so
+func (stack *Stack) delete(cf *cloudformation.CloudFormation) error {
 	_, err := cf.DeleteStack(&cloudformation.DeleteStackInput{
-		StackName: aws.String(stackID),
+		StackName: aws.String(stack.ID),
 	})
 	return err
 }
 func main() {
 	var templateLocation = flag.String("template", "", "the file location of the template")
+	var stackName = flag.String("name", "CloudValidate", "the name of the stack to create. Defaults to CloudValidate")
+	var persist = *flag.Bool("persist", false, "persist will persist the stack if successful. Defaults to false, deleting the stack after completion")
 	flag.Parse()
+	fmt.Println(persist)
 	if *templateLocation == "" {
 		color.Red("Please specify the location of the template file")
 		os.Exit(1)
@@ -81,6 +91,11 @@ func main() {
 		color.Red(err.Error())
 		os.Exit(1)
 	}
+	stack := &Stack{
+		Template: template,
+		Name:     *stackName,
+		ID:       "",
+	}
 	session, err := session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1"),
 	})
@@ -89,20 +104,24 @@ func main() {
 		os.Exit(1)
 	}
 	cf := cloudformation.New(session, aws.NewConfig().WithRegion(*session.Config.Region))
-	stackID, err := createStack(cf, template)
+	stackID, err := stack.create(cf)
 	if err != nil {
 		color.Red("Failed to create stack")
 		color.Red(err.Error())
 		os.Exit(1)
 	}
-	err = describeStack(cf, stackID)
+	stack.ID = stackID
+	err = stack.describe(cf)
 	for err != nil {
 		color.Red("Failed to describe stack")
 		color.Red(err.Error())
-		err = describeStack(cf, stackID)
+		err = stack.describe(cf)
+	}
+	if persist == true {
+		return
 	}
 	color.Yellow("Deleting stack")
-	if err = deleteStack(cf, stackID); err != nil {
+	if err = stack.delete(cf); err != nil {
 		color.Red(err.Error())
 	}
 }
