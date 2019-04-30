d2f1eac861f698987a423c265b3adaeb6c8cd5c2
Chris <diehl.chris24@gmail.com>
2019-04-30 16:12:41 -0400

    refactor to use a single describe stack

--- main.go
+++ main.go
@@ -66,52 +66,48 @@ func (stack *Stack) create(cf *cloudformation.CloudFormation) (string, error) {
 // describe checks the current status of the stack
 // returns bool to indicate if successfully created and an error to describe the stack failure
 func (stack *Stack) describe(cf *cloudformation.CloudFormation) (bool, error) {
-	for {
-		dsOut, err := cf.DescribeStacks(&cloudformation.DescribeStacksInput{
-			NextToken: aws.String("1"),
-			StackName: aws.String(stack.ID),
-		})
-		for i, cfStack := range dsOut.Stacks {
-			if i > 0 {
-				return false, StackError{
-					Stack:       stack,
-					Reason:      "Multiple stacks returned",
-					Status:      "AWS ERROR",
-					StackEvents: nil,
-				}
-			}
-			fmt.Println(cfStack)
-			if *cfStack.StackStatus == "CREATE_COMPLETE" {
-				return true, nil
-			} 
-			if *cfStack.StackStatus == "CREATE_FAILED"
-
+	dsOut, err := cf.DescribeStacks(&cloudformation.DescribeStacksInput{
+		NextToken: aws.String("1"),
+		StackName: aws.String(stack.ID),
+	})
+	if err != nil {
+		return false, StackError{
+			Stack:       stack,
+			Reason:      err.Error(),
+			Status:      "AWS ERROR",
+			StackEvents: nil,
 		}
-		out, err := cf.DescribeStackEvents(&cloudformation.DescribeStackEventsInput{
-			NextToken: aws.String("1"),
-			StackName: aws.String(stack.ID),
-		})
-
-		if err != nil {
-			fmt.Println(err.Error())
-			return false, err
-		}
-		for _, event := range out.StackEvents {
-			if *event.ResourceStatus == "CREATE_FAILED" {
-				return false, StackError{
-					Stack:       stack,
-					Reason:      *event.ResourceStatusReason,
-					Status:      *event.ResourceStatus,
-					StackEvents: out.StackEvents,
-				}
-			}
-			if *event.ResourceStatus == "CREATE_COMPLETE" {
-				return true, nil
-			}
-		}
-		return false, nil
 	}
-
+	for i, cfStack := range dsOut.Stacks {
+		if i > 0 {
+			return false, StackError{
+				Stack:       stack,
+				Reason:      "Multiple stacks returned",
+				Status:      "AWS ERROR",
+				StackEvents: nil,
+			}
+		}
+		fmt.Println(*cfStack.StackStatus)
+		if *cfStack.StackStatus == "CREATE_COMPLETE" {
+			return true, nil
+		}
+		if *cfStack.StackStatus == "ROLLBACK_INITIATED" {
+			out, err := cf.DescribeStackEvents(&cloudformation.DescribeStackEventsInput{
+				NextToken: aws.String("1"),
+				StackName: aws.String(stack.ID),
+			})
+			if err != nil {
+				return false, err
+			}
+			return false, StackError{
+				Stack:       stack,
+				Reason:      "Multiple stacks returned",
+				Status:      *cfStack.StackStatus,
+				StackEvents: out.StackEvents,
+			}
+		}
+	}
+	return false, nil
 }
 
 // delete deletes the current stack and returns an error if failing to do so
